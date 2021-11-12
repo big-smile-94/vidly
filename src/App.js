913164f4c3ccfa400c3ce1704d2c37ec1230e220
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-12 04:07:57 +0000

    add a basic hardcoded pagination component to movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Movies from './components/movies';
+import Pagination from './components/common/pagination';
 import './App.css';
 
 function App() {
   return (
     <main className="container">
       <Movies />
+      <Pagination />
     </main>
   );
 }
