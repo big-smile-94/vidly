ec8771e1b7e2a0d53ce8c270c355fdfe1f737c50
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-14 08:22:03 +0000

    make the table fully generic so it's easy to resuse for any other types like customers etc

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Like from './common/like';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
-
+import Table from './common/table';
 class MoviesTable extends Component {
   columns = [
     { label: 'Title', path: 'title' },
@@ -29,14 +27,12 @@ class MoviesTable extends Component {
   render() {
     const { movies, onSort, sortColumn } = this.props;
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+        data={movies}
+      />
     );
   }
 }
