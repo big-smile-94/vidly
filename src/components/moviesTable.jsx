431d23535c7c9423bf56f4101c55b2c89fa42791
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-14 06:12:24 +0000

    move moives table header to it's own component and make it generic

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,34 +1,26 @@
 import React, { Component } from 'react';
 import Like from './common/like';
+import TableHeader from './common/tableHeader';
+
+const columns = [
+  { label: 'Title', path: 'title' },
+  { label: 'Genre', path: 'genre.name' },
+  { label: 'Stock', path: 'numberInStock' },
+  { label: 'Rate', path: 'dailyRentalRate' },
+  {},
+  {},
+];
 
 class MoviesTable extends Component {
-  raiseSort = (path) => {
-    const sortColumn = { ...this.props.sortColumn };
-
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
-    else {
-      sortColumn.path = path;
-      sortColumn.order = 'asc';
-    }
-
-    this.props.onSort(sortColumn);
-  };
-
   render() {
-    const { movies, onLike, onDelete } = this.props;
+    const { movies, onLike, onDelete, onSort, sortColumn } = this.props;
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => this.raiseSort('title')}>Title</th>
-            <th onClick={() => this.raiseSort('genre.name')}>Genre</th>
-            <th onClick={() => this.raiseSort('numberInStock')}>Stock</th>
-            <th onClick={() => this.raiseSort('dailyRentalRate')}>Rate</th>
-            <th />
-            <th />
-          </tr>
-        </thead>
+        <TableHeader
+          columns={columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
         <tbody>
           {movies.map((movie) => (
             <tr key={movie._id}>
