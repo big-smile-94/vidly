65c9b6f7fd50c09cb527b47c6027a573dd54b6b2
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-20 21:00:01 +0000

    add login link and form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/common/navBar';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
+import LoginForm from './components/loginForm';
 import './App.css';
 
 function App() {
@@ -13,10 +14,11 @@ function App() {
       <NavBar />
       <main className="container">
         <Routes>
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/rentals" element={<Rentals />} />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/movies/:id" index element={<MovieForm />} />
           <Route path="/movies" element={<Movies />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/rentals" element={<Rentals />} />
           <Route path="/not-found" element={<NotFound />} />
           <Route path="/" element={<Navigate to="/movies" />} />
           <Route path="*" element={<NotFound />} />
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // old way of accessing form data in vanilla javascript
+    const username = document.getElementById('username').value;
+
+    console.log('submited');
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
