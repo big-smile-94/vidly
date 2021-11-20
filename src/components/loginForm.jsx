ada64fbfa745441346e917ae66e6135649d48d75
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-20 21:50:14 +0000

    implement validation and display validation messages

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import Input from './common/input';
 
 class LoginForm extends Component {
   state = {
     account: { username: '', password: '' },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === '')
+      errors.username = 'Username is required.';
+    if (account.password.trim() === '')
+      errors.password = 'Password is required.';
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+
+    if (errors) return;
 
     console.log('submited');
   };
@@ -18,31 +37,27 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            type="text"
+            onChange={this.handleChange}
+            error={errors.username}
+          />
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              value={account.username}
-              onChange={this.handleChange}
-              id="username"
-              name="username" // to be able to use the dynamic bracket notation to set the target property of the account object in the handle the change event
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
+            <Input
+              name="password"
               value={account.password}
-              onChange={this.handleChange}
-              id="password"
-              name="password" // to be able to use the dynamic bracket notation to set the target property of the account object in the handle the change event
+              label="Password"
               type="password"
-              className="form-control"
+              onChange={this.handleChange}
+              error={errors.password}
             />
           </div>
           <button className="btn btn-primary">Login</button>
