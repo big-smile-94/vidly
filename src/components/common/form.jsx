ef24ed0610f7b57a12b6e2c0fd7ac84160bf6e9a
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-20 23:24:00 +0000

    refactor loginForm and extract reusable code into form

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
+import Input from './input';
 
 class Form extends Component {
   state = {
@@ -54,6 +55,29 @@ class Form extends Component {
     data[input.name] = input.value;
     this.setState({ data, errors });
   };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderInput(name, label, type = 'text') {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        name={name}
+        value={data[name]}
+        label={label}
+        type={type}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
 }
 
 export default Form;
