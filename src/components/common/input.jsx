0b81dd70eb412bc999cabce787e64484f6057812
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-20 21:50:06 +0000

    move input to it's own component

---
+++ src/components/common/input.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Input = ({ name, label, value, onChange, type, error }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        value={value}
+        onChange={onChange}
+        id={name}
+        name={name} // to be able to use the dynamic bracket notation to set the target property of the account object in the handle the change event
+        type={type}
+        className="form-control"
+      />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
