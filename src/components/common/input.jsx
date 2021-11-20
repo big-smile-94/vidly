dec062f1ac65c89f0b2f107eadedc53dc397c1b4
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-20 23:30:25 +0000

    refactor input component to spread the properties that are the same so we don't have to keep on adding them when we add new properties

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+// before = const Input = ({ name, label, value, onChange, type, error }) =>
+// <input value={value} onChange={onChange} name={name} type={type} id={name} className="form-control" />
 
-const Input = ({ name, label, value, onChange, type, error }) => {
+// after = const Input = ({ name, label, error, ...rest }) =>
+// <input {...reset} name={name} id={name} className="form-control" />
+
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        name={name} // to be able to use the dynamic bracket notation to set the target property of the account object in the handle the change event
-        type={type}
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
 
 export default Input;
+
+// <input we use the name property because of the following
+//to be able to use the dynamic bracket notation to set the target property of the account object in the handle the change event
