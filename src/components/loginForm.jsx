27cfe7bd001470536e1f472d70a9c5588a6896b1
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-20 21:09:48 +0000

    use of ref and autoFocus! we should avoid using ref unless working with third party libararies to work with the document! this is jus to show how it works

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  username = React.createRef();
+
+  //   componentDidMount() {
+  //     this.username.current.focus();
+  //   }
+
   handleSubmit = (e) => {
     e.preventDefault();
 
     // old way of accessing form data in vanilla javascript
-    const username = document.getElementById('username').value;
+    // const username = document.getElementById('username').value;
+    // if you really have to work with the 'document'. you can use the ref
+    // 1. username = React.createRef(); 2.  <input ref={this.username}.../> 3. const u = this.username.current.value
 
     console.log('submited');
   };
@@ -17,7 +25,13 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
