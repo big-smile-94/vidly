13f290f47d61cf1b6125c457d6988a13c6fa744d
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-25 22:52:12 +0000

    refactor protected routes and redirect user to the page the were on before we redirected them to login page because they did not have permissions.

---
+++ src/components/common/protectedRoute.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import auth from '../../services/authService';
+
+const ProtectedRoute = (props) => {
+  const { component: Component, render, ...rest } = props;
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!auth.getCurrentUser())
+          return (
+            <Redirect
+              to={{
+                pathname: '/login',
+                state: { from: props.location },
+              }}
+            />
+          );
+        return Component ? <Component {...props} /> : render(props);
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
