100747493dbfd9398edf1ed2d444b57fa95804a4
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-25 22:33:33 +0000

    hide new movie button when user is logged out and show it when user is logged in

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -119,6 +119,7 @@ class Movies extends Component {
       genres,
       searchQuery,
     } = this.state;
+    const { user } = this.props;
 
     if (!count) return <p>There are no movies in the database.</p>;
 
@@ -134,13 +135,15 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link
-            to="/movies/new"
-            className="btn btn-primary"
-            style={{ marginBottom: 20 }}
-          >
-            New Movie
-          </Link>
+          {user && (
+            <Link
+              to="/movies/new"
+              className="btn btn-primary"
+              style={{ marginBottom: 20 }}
+            >
+              New Movie
+            </Link>
+          )}
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
